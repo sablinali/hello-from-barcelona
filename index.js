ca71d3c7607e858e0dfe6336fc6bb236e658020e
sablinali <sablinahare@gmail.com>
2024-04-28 15:36:14 +0200

    added light mode button

--- index.js
+++ index.js
@@ -1,25 +1,36 @@
+function changeMainTitle() {
+  const mainTitleEl = document.querySelector(".mainTitle");
+  console.log(mainTitleEl.textContent);
 
-function changeMainTitle(){
-    const mainTitleEl = document.querySelector(".mainTitle");
-    console.log(mainTitleEl.textContent);
+  mainTitleEl.textContent = "Hello from Barcelona!";
+  mainTitleEl.style.backgroundColor = "pink";
 
-    mainTitleEl.textContent = "Hello from Barcelona!";
-    mainTitleEl.style.backgroundColor = "pink";
-
-    console.log(mainTitleEl.textContent);
+  console.log(mainTitleEl.textContent);
 }
 
-//changeMainTitle();
+//seconds to changeMainTitle();
 const greeting = setTimeout(changeMainTitle, 3000);
 
-function darkMode(){
-   document.body.style.backgroundColor = "black";
-   const textElement = document.querySelector(".text");
-   if (textElement) {
-       textElement.style.color = "rgb(230, 230, 250)";
-   }
-   const image = document.querySelector(".photo");
-   console.log(image.src);
-   image.src = "./moi.jpg";
-   
-};
+//change to dark mode
+function darkMode() {
+  document.body.style.backgroundColor = "black";
+  const textElement = document.querySelector(".text");
+  if (textElement) {
+    textElement.style.color = "white";
+  }
+  const image = document.querySelector(".photo");
+  console.log(image.src);
+  image.src = "./moi.jpg";
+}
+
+//cahnge to light mode
+function lightMode() {
+  document.body.style.backgroundColor = "white";
+  const textElement = document.querySelector(".text");
+  if (textElement) {
+    textElement.style.color = "maroon";
+  }
+  const image = document.querySelector(".photo");
+  console.log(image.src);
+  image.src = "./sablina_sagrada_familia.jpg";
+}
